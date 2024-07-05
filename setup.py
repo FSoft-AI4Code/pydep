2588e42523ab1ff8a596a5c629df7ed6fee56bb0
NamCyan <nkim@gmail.com>
2024-07-05 20:40:21 +0700

    update lib

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "py-dependency",
-    version = "0.0.1",
+    name = "pycall",
+    version = "0.0.5",
     author = "Nam Le Hai",
     author_email = "nam.lh173264@gmail.com",
     description = "Extract dependencies of modules (function, class, import) in python repository",
@@ -13,8 +13,13 @@ setuptools.setup(
     long_description_content_type = "text/markdown",
     url = "https://github.com/FSoft-AI4Code/py-dependency",
     project_urls = {
-        "Homepage" = "https://github.com/FSoft-AI4Code/py-dependency",
+        "repository": "https://github.com/FSoft-AI4Code/py-dependency",
     },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     package_dir = {"": "src"},
     packages = setuptools.find_packages(where="src"),
     python_requires = ">=3.10"
