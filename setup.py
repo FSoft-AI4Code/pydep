737a2708ca346139b5622b74ed6fd25d7a30c1c9
NamCyan <nkim@gmail.com>
2024-07-07 23:40:37 +0700

    update

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "pycall",
-    version = "0.0.6",
+    name = "pydepcall",
+    version = "0.0.1",
     author = "Nam Le Hai",
     author_email = "nam.lh173264@gmail.com",
     description = "Extract dependencies of modules (function, class, import) in python repository",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "https://github.com/FSoft-AI4Code/py-dependency",
+    url = "https://github.com/FSoft-AI4Code/pydepcall",
     project_urls = {
-        "repository": "https://github.com/FSoft-AI4Code/py-dependency",
+        "repository": "https://github.com/FSoft-AI4Code/pydepcall",
     },
     classifiers = [
         "Programming Language :: Python :: 3",
